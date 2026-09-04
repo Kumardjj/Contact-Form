1ded06e6b39f444e919a6da5389aa7a714cee5fb
Dheeraj Kumar <kumardjj2001@gmail.com>
2026-09-04 13:04:28 +0530

    whatapp_twilio

---
+++ app/notification/whatsapp.py
@@ -0,0 +1,10 @@
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+# TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+
+# TWILIO_WHATSAPP_FROM = os.getenv("TWILIO_WHATSAPP_FROM")
+# TWILIO_WHATSAPP_TO = os.getenv("TWILIO_WHATSAPP_TO")
